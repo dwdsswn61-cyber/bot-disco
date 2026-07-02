8a57ff86c3cc8e386ca749d8938c03a9785b342c
dwdsswn61-cyber <dwdsswn61@gmail.com>
2026-07-02 19:21:09 +0300

    Reorganize imports and server setup in index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const { Client, GatewayIntentBits } = require("discord.js");
 const express = require("express");
+const { Client, GatewayIntentBits } = require("discord.js");
 
 const app = express();
 
-// חשוב מאוד - חייב לרוץ לפני הכל
 app.get("/", (req, res) => {
   res.status(200).send("OK");
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("HTTP server is running");
+// חייב להיות לפני הבוט כדי ש-Render יזהה פורט
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("HTTP server running on port", PORT);
 });
 
 const client = new Client({
@@ -20,12 +21,12 @@ const client = new Client({
   ]
 });
 
-client.once("ready", () => {
-  console.log("BOT ONLINE:", client.user.tag);
-});
-
 require("./ticket.js")(client);
 require("./casino.js")(client);
 require("./daily.js")(client);
+
+client.once("ready", () => {
+  console.log("BOT ONLINE:", client.user.tag);
+});
 
 client.login(process.env.DISCORD_TOKEN);
